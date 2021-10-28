4747cc206a03c011576d9fc7d0bf421408caa608
Hramcov Semen <gerasm666999@gmail.com>
2021-10-28 14:14:57 +0500

    first commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int price = 3750;
+        int x = 20;
+        int bonus = price / x;
+        System.out.println("Количество начисленных баллов - "+bonus);
+    }
+}
